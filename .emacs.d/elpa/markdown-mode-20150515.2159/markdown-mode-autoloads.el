8822c2b691cc72b6c962184d33292d2af0affe80
JesusMtnez <jesusmartinez93@gmail.com>
2015-05-19 19:04:49 +0200

    Update emacs packages.

--- .emacs.d/elpa/markdown-mode-20150515.2159/markdown-mode-autoloads.el
+++ .emacs.d/elpa/markdown-mode-20150515.2159/markdown-mode-autoloads.el
@@ -3,7 +3,7 @@
 ;;; Code:
 (add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
 
-;;;### (autoloads nil "markdown-mode" "markdown-mode.el" (21848 40493
+;;;### (autoloads nil "markdown-mode" "markdown-mode.el" (21851 892
 ;;;;;;  0 0))
 ;;; Generated autoloads from markdown-mode.el
 
