f4462d4450a875282e17444654ee77bccbee4f82
JesusMtnez <jesusmartinez93@gmail.com>
2015-07-27 19:01:22 +0200

    Enable 'helm' in emacs with a simple configuration.

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -9,6 +9,37 @@
 (unless package-archive-contents (package-refresh-contents))
 (package-install 'paradox)
 
+(package-install 'helm)
+(require 'helm)
+(require 'helm-config)
+
+(global-set-key (kbd "C-c h") 'helm-command-prefix)
+(global-unset-key (kbd "C-x c"))
+(global-set-key (kbd "M-x") 'helm-M-x) ; replace M-x with helm-M-X
+(global-set-key (kbd "M-y") 'helm-show-kill-ring)
+(global-set-key (kbd "C-x b") 'helm-mini)
+(global-set-key (kbd "C-x C-f") 'helm-find-files)
+
+(define-key helm-map (kbd "<tab>") 'helm-execute-persistent-action) ; rebind tab to run persistent action
+(define-key helm-map (kbd "C-i") 'helm-execute-persistent-action) ; make TAB works in terminal
+(define-key helm-map (kbd "C-z")  'helm-select-action) ; list actions using C-z
+
+(when (executable-find "curl")
+  (setq helm-google-suggest-use-curl-p t))
+
+(setq helm-split-window-in-side-p t
+      helm-move-to-line-cycle-in-source t
+      helm-ff-search-library-in-sexp t
+      helm-scroll-amount 8
+      helm-ff-file-name-history-use-recentf t
+      helm-autoresize-mode t;
+      helm-autoresize-max-height 20
+      helm-M-x-fuzzy-match t
+      helm-buffers-fuzzy-matching t
+      helm-recentf-fuzzy-match t)
+
+(helm-mode t)
+
 ;; ido-vertical-mode package
 ;; (paradox-require 'ido-vertical-mode)
 ;; (ido-mode t)
