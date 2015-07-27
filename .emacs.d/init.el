42bc37d9a4c501e51f1f3fd574527d15c4fee3f5
JesusMtnez <jesusmartinez93@gmail.com>
2015-07-27 22:42:10 +0200

    Change 'helm-mini' to 'helm-buffer-list'.

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -16,9 +16,9 @@
 (global-set-key (kbd "C-c h") 'helm-command-prefix)
 (global-unset-key (kbd "C-x c"))
 (global-set-key (kbd "M-x") 'helm-M-x) ; replace M-x with helm-M-X
+(global-set-key (kbd "C-x C-f") 'helm-find-files) ; replace find-file
 (global-set-key (kbd "M-y") 'helm-show-kill-ring)
-(global-set-key (kbd "C-x b") 'helm-mini)
-(global-set-key (kbd "C-x C-f") 'helm-find-files)
+(global-set-key (kbd "C-x b") 'helm-buffer-list)
 
 (define-key helm-map (kbd "<tab>") 'helm-execute-persistent-action) ; rebind tab to run persistent action
 (define-key helm-map (kbd "C-i") 'helm-execute-persistent-action) ; make TAB works in terminal
