166ccfa56c6245f795911b5674290da5f1334f66
JesusMtnez <jesusmartinez93@gmail.com>
2015-09-05 19:34:44 +0200

    Update 'install.sh'.

--- install.sh
+++ install.sh
@@ -1,38 +1,17 @@
 ############################################
 # Author: Jesús Martínez-Barquero Herrrada #
-# Last update: 04/09/2015                  #
+# Last update: 05/09/2015                  #
 ############################################
 #!/usr/bin/env sh
 
 DOT_DIR="$HOME/.dotfiles"
-DOT_FILES="gitconfig tmux.conf zshrc Xmodmap"
-DOT_DIRS="emacs.d"
+DOT_FILES="gitconfig tmux.conf zshrc Xmodmap emacs.d"
 
-###########################
-# Delete old config files #
-###########################
 for f in $DOT_FILES; do
-    if [ -f $HOME/.$f ]; then
+    if [ -e $HOME/.$f ]; then
       rm $HOME/.$f;
     fi
-done
-
-for d in $DOT_DIRS; do
-    if [ -d $HOME/.$d ]; then
-      rm -rf $HOME/.$d;
-    fi
-done
-
-####################
-# Install dotfiles #
-####################
-# Install files
-for f in $DOT_FILES; do
     ln -sf $DOT_DIR/$f $HOME/.$f
-done
-# Install dirs
-for d in $DOT_DIRS; do
-    ln -sf $DOT_DIR/$d $HOME/.$d
 done
 
 echo "JesusMtnez's dotfiles installed correctly. Enjoy!!"
