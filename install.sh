3327bd90b49c5463a38067c1c97c117849e426d4
JesusMtnez <jesusmartinez93@gmail.com>
2015-09-16 22:38:55 +0200

    Update 'install.sh' with bin/ folder.

--- install.sh
+++ install.sh
@@ -1,8 +1,5 @@
-############################################
-# Author: Jesús Martínez-Barquero Herrrada #
-# Last update: 07/09/2015                  #
-############################################
 #!/usr/bin/env sh
+# Author: JesusMtnez
 
 DOT_DIR="$HOME/.dotfiles"
 DOT_FILES="gitconfig tmux.conf zshrc Xmodmap emacs.d"
@@ -12,6 +9,17 @@ for f in $DOT_FILES; do
       rm -rf $HOME/.$f;
     fi
     ln -sf $DOT_DIR/$f $HOME/.$f
+done
+
+# Create $HOME/bin if necessary
+if [[ ! -d $HOME/bin ]]; then
+  mkdir $HOME/bin
+fi
+
+# Install my scripts
+for b in bin/*; do
+    chmod +x $DOT_DIR/$b
+    ln -sf $DOT_DIR/$b $HOME/$b
 done
 
 echo "JesusMtnez's dotfiles installed correctly. Enjoy!!"
