9f66db20625d70e89495351a08371fc28857b0c4
JesusMtnez <jesusmartinez93@gmail.com>
2016-05-20 18:16:56 +0200

    [ZSH] Clean functions.zsh format

--- zsh/functions.zsh
+++ zsh/functions.zsh
@@ -1,14 +1,14 @@
 # Create dir and move inside.
 mk() {
-    mkdir -p "$1" && cd "$1"
+  mkdir -p "$1" && cd "$1"
 }
 
 # Act as git status if no parameters.
 g() {
-    if [[ $# > 0 ]]; then
-        git $@
-    else
-        git status -sb
-    fi
+  if [[ $# > 0 ]]; then
+    git $@
+  else
+    git status -sb
+  fi
 }
 compdef g=git
