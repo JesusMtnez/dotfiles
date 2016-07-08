0d8798903243601a1ec5bc1c3d12ab2a94d1360a
JesusMtnez <jesusmartinez93@gmail.com>
2016-07-08 16:58:30 +0200

    [SH] Link i3 configuration in install.sh

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 # Author: JesusMtnez
 
 DOT_DIR="$HOME/.dotfiles"
-DOT_FILES="gitconfig gitignore tmux.conf zshrc Xmodmap spacemacs.d"
+DOT_FILES="gitconfig gitignore tmux.conf zshrc Xmodmap spacemacs.d i3"
 
 for f in $DOT_FILES; do
     if [ -e $HOME/.$f ]; then
