50d93481f4ec26685d79b74c5d8b0922b3865a2e
JesusMtnez <jesusmartinez93@gmail.com>
2020-08-18 12:24:14 +0200

    [NIX] Add VSCodium installation with settings

--- home.nix
+++ home.nix
@@ -11,6 +11,40 @@
     package = pkgs.emacs;
   };
 
+  programs.vscode = {
+    enable = true;
+    package = pkgs.vscodium;
+    userSettings = {
+      "editor.fontFamily" = "'FuraCode Nerd Font'";
+      "editor.fontLigatures" = true;
+      "editor.fontSize" = 12;
+      "editor.fontWeight" = "normal";
+      "editor.renderWhitespace" = "all";
+      "editor.tabSize" = 2;
+      "editor.wordWrapColumn" = 100;
+      "explorer.confirmDelete" = false;
+      "files.autoSave" = "afterDelay";
+      "files.autoSaveDelay" = 500;
+      "files.insertFinalNewline" = true;
+      "files.trimFinalNewlines" = true;
+      "files.trimTrailingWhitespace" = true;
+      "oneDarkPro.vivid" = true;
+      "terminal.integrated.cursorStyle" = "line";
+      "terminal.integrated.fontFamily" = "'FuraCode Nerd Font'";
+      "terminal.integrated.fontSize" = 12;
+      "window.restoreFullscreen" = true;
+      "window.zoomLevel" = 0;
+      "workbench.iconTheme" = "material-icon-theme";
+      "workbench.colorTheme" = "One Dark Pro";
+      "liveshare.anonymousGuestApproval" = "accept";
+      "metals.bloopSbtAlreadyInstalled" = true;
+      "metals.serverVersion" = "0.9.2";
+    };
+    # TODO Manage extensions
+    # https://nixos.wiki/wiki/VSCodium
+    # extensions = with pkgs.vscode-extensions; [];
+  };
+
   home.packages = with pkgs; [
     feh
     direnv
