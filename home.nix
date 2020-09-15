c7efd34690763556838461fd40a982f0007e1c21
JesusMtnez <jesusmartinez93@gmail.com>
2020-09-15 15:49:04 +0200

    [NIX] Add jitsi-meet-electron app

--- home.nix
+++ home.nix
@@ -72,6 +72,8 @@ in
 
     # TODO Handle tpm / plugins installation
     # TODO zsh / zplug integration https://github.com/rycee/home-manager/blob/1b210e7143547ce0f41e8082b8d27e9c7d220351/modules/programs/zplug.nix
+
+    jitsi-meet-electron
   ];
 
   # TODO Service is not started: https://github.com/rycee/home-manager#graphical-services
