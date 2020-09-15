41a258aed7840dc4402eeafcf63028382d364ac8
JesusMtnez <jesusmartinez93@gmail.com>
2020-09-15 17:31:16 +0200

    [NIX] Remove jitsi-meet-electron

--- home.nix
+++ home.nix
@@ -73,7 +73,6 @@ in
     # TODO Handle tpm / plugins installation
     # TODO zsh / zplug integration https://github.com/rycee/home-manager/blob/1b210e7143547ce0f41e8082b8d27e9c7d220351/modules/programs/zplug.nix
 
-    jitsi-meet-electron
     keepassxc
     slack
   ];
