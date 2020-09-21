0fc67841493ad41335aca4203595c0d396b584e3
JesusMtnez <jesusmartinez93@gmail.com>
2020-09-21 15:49:39 +0200

    [NIX][i3] Fetch theme from official repository

--- applications/rofi/default.nix
+++ applications/rofi/default.nix
@@ -1,7 +1,14 @@
-{ ... }:
-{
+{ pkgs, ... }:
+let
+  rofiThemes = pkgs.fetchFromGitHub {
+    owner = "davatorium";
+    repo = "rofi-themes";
+    rev = "e17b591590f0445fdc8ec1ff876f26d994e99bb4";
+    sha256 = "1n7wb6dhcjzjy8dwpkrhwn3ckdi6m3cddmk8vphvaxn202mgpqvi";
+  };
+in {
   programs.rofi.enable = true;
 
   xdg.configFile."rofi/config.rasi".source = ./config.rasi;
-  xdg.configFile."rofi/slate.rasi".source = ./slate.rasi;
+  xdg.configFile."rofi/slate.rasi".text = builtins.readFile ( rofiThemes + "/User Themes/slate.rasi" );
 }
