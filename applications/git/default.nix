078ef3603acb12aaf63d6c51fe61b2562099cef3
JesusMtnez <jesusmartinez93@gmail.com>
2020-10-25 17:20:48 +0100

    [NIX] Use gh from home-manager module

--- applications/git/default.nix
+++ applications/git/default.nix
@@ -3,9 +3,14 @@
   home.packages = with pkgs; [
     git
     gitAndTools.diff-so-fancy
-    gitAndTools.gh
   ];
 
   home.file.".gitconfig".source = ./gitconfig;
   home.file.".gitignore".source = ./gitignore;
+
+  programs.gh = {
+    enable = true;
+    editor = "emacs";
+    gitProtocol = "ssh";
+  };
 }
