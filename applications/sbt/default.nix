3bcd07675988c69b96ff780c9c9b8c40a53c414a
JesusMtnez <jesusmartinez93@gmail.com>
2020-11-17 09:39:24 +0100

    [SBT] Bump scalafix to 0.9.23

--- applications/sbt/default.nix
+++ applications/sbt/default.nix
@@ -6,6 +6,6 @@
     addSbtPlugin("org.jmotor.sbt"   % "sbt-dependency-updates"    % "1.2.2")
     addSbtPlugin("com.github.cb372" % "sbt-explicit-dependencies" % "0.2.13")
     addSbtPlugin("io.spray"         % "sbt-revolver"              % "0.9.1")
-    addSbtPlugin("ch.epfl.scala"    % "sbt-scalafix"              % "0.9.21")
+    addSbtPlugin("ch.epfl.scala"    % "sbt-scalafix"              % "0.9.23")
   '';
 }
