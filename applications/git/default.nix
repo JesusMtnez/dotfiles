149f76759e985145df8932519bb8fcd97ae27b94
JesusMtnez <jesusmartinez93@gmail.com>
2021-02-19 05:39:55 +0100

    [NIX] Install gh from nixpkgs directly

--- applications/git/default.nix
+++ applications/git/default.nix
@@ -3,14 +3,9 @@
   home.packages = with pkgs; [
     git
     gitAndTools.diff-so-fancy
+    gitAndTools.gh
   ];
 
   home.file.".gitconfig".source = ./gitconfig;
   home.file.".gitignore".source = ./gitignore;
-
-  programs.gh = {
-    enable = true;
-    editor = "emacs";
-    gitProtocol = "ssh";
-  };
 }
