0b4c018cf8f77e2ec1df2e02fe27703ded14218a
JesusMtnez <jesusmartinez93@gmail.com>
2021-02-23 05:39:02 +0100

    [SBT] Use home-manager integration

--- applications/sbt/default.nix
+++ applications/sbt/default.nix
@@ -1,18 +1,37 @@
 { pkgs, ... }:
 {
 
-  home.packages = [
-    (pkgs.sbt.override { jre = pkgs.jdk11_headless; })
-  ];
-
-  home.file.".sbt/1.0/plugins/globalPlugins.sbt".text = ''
-    addSbtPlugin("ch.epfl.scala"    % "sbt-bloop"                 % "1.4.8")
-    //addSbtPlugin("net.virtual-void" % "sbt-dependency-graph"      % "0.10.0-RC1")
-    addSbtPlugin("org.jmotor.sbt"   % "sbt-dependency-updates"    % "1.2.2")
-    addSbtPlugin("com.github.cb372" % "sbt-explicit-dependencies" % "0.2.16")
-    addSbtPlugin("io.spray"         % "sbt-revolver"              % "0.9.1")
-    addSbtPlugin("ch.epfl.scala"    % "sbt-scalafix"              % "0.9.25")
-  '';
+  programs.sbt = {
+    enable = true;
+    package = (pkgs.sbt.override { jre = pkgs.jdk11_headless; });
+    plugins = [
+      {
+        org = "ch.epfl.scala";
+        artifact = "sbt-bloop";
+        version = "1.4.8";
+      }
+      {
+        org = "org.jmotor.sbt";
+        artifact = "sbt-dependency-updates";
+        version = "1.2.2";
+      }
+      {
+        org = "com.github.cb372";
+        artifact = "sbt-explicit=dependencies";
+        version = "0.2.16";
+      }
+      {
+        org = "io.spray";
+        artifact = "sbt-revolver";
+        version = "0.9.1";
+      }
+      {
+        org = "ch.epfl.scala" ;
+        artifact = "sbt-scalafix";
+        version = "0.9.25";
+      }
+    ];
+  };
 
   home.file.".sbt/1.0/global.sbt".text = ''
     credentials += Credentials(Path.userHome / ".sbt" / ".credentials")
