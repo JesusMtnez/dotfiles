821d5e65c060233bf36d590229ebbccd969f470e
JesusMtnez <jesusmartinez93@gmail.com>
2021-12-02 05:39:08 +0100

    update vscode extensions

--- applications/code/managed.nix
+++ applications/code/managed.nix
@@ -8,8 +8,8 @@
   {
     name = "rest-client";
     publisher = "humao";
-    version = "0.24.5";
-    sha256 = "1hj294nsmlzvhbvwv4wyf7mgfw64q4pgkjzzgyjfc26pzyaxb4bn";
+    version = "0.24.6";
+    sha256 = "196pm7gv0488bpv1lklh8hpwmdqc4yimz389gad6nsna368m4m43";
   }
   {
     name = "nix-ide";
@@ -26,20 +26,20 @@
   {
     name = "material-icon-theme";
     publisher = "PKief";
-    version = "4.10.0";
-    sha256 = "119zpdx2hx9d1xb3d9d773b88i2awr62ivmmnhxm9zp77x9y6b70";
+    version = "4.11.0";
+    sha256 = "1l2s8j645riqjmj09i3v71s8ycin5vd6brdp35z472fnk6wyi1y6";
   }
   {
     name = "ansible";
     publisher = "redhat";
-    version =  "0.7.0";
-    sha256 = "1ryzywcywlkpdd3qnrjmlxbls89q5iqxbs9qh3r2vhm071hhm2hb";
+    version =  "0.7.1";
+    sha256 = "18z9h80y2ci391gyvv3f28lqs6l7rh1c0pidamksczcc3h5minhg";
   }
   {
     name = "vscode-yaml";
     publisher = "redhat";
-    version = "1.2.0";
-    sha256 = "0s68a6h9ri6bwinrd94ppxpcmklvp77gxvrjxkhg3gr5q7377hiy";
+    version = "1.2.2";
+    sha256 = "06n4fxqr3lqmiyns9jdk3rdnanamcpzhrivllai8z9d997xmwcx6";
   }
   {
     name = "scala";
@@ -56,7 +56,7 @@
   {
     name = "material-theme";
     publisher = "zhuangtongfa";
-    version = "3.13.2";
-    sha256 = "1pb96xdm8342i1l4xp7wdbpfv353pjf6l0c3xkgw0lgfzvd0xshy";
+    version = "3.13.6";
+    sha256 = "1rlfgl7gzph6xyhl9nrbrzqc26x99incdfd90f93q2pybn5km86z";
   }
 ]
