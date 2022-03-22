fe59b87900c09377b98be1d909fdad59a8e58c94
JesusMtnez <jesusmartinez93@gmail.com>
2022-03-22 07:51:44 +0100

    fmt scripts

--- scripts/default.nix
+++ scripts/default.nix
@@ -2,40 +2,41 @@
 
 let
   dotTool = with pkgs; writeScriptBin "dot" ''
-#!${runtimeShell}
+    #!${runtimeShell}
 
-pushd $HOME/.dotfiles > /dev/null
+    pushd $HOME/.dotfiles > /dev/null
 
-case $1 in
-  "fmt")
-    FILES=$(${git}/bin/git ls-files '*.nix' | grep -v 'nix/.*' | tr '\n' ' ')
-    ${nixpkgs-fmt}/bin/nixpkgs-fmt $FILES
-  ;;
+    case $1 in
+      "fmt")
+        FILES=$(${git}/bin/git ls-files '*.nix' | grep -v 'nix/.*' | tr '\n' ' ')
+        ${nixpkgs-fmt}/bin/nixpkgs-fmt $FILES
+      ;;
 
-  "update")
-    nix-shell --run "niv update"
-  ;;
+      "update")
+        nix-shell --run "niv update"
+      ;;
 
-  "build")
-    nix-shell --run "home-manager build"
-  ;;
+      "build")
+        nix-shell --run "home-manager build"
+      ;;
 
-  "switch")
-    nix-shell --run "home-manager switch"
-  ;;
+      "switch")
+        nix-shell --run "home-manager switch"
+      ;;
 
-  "news")
-    nix-shell --run "home-manager news"
-  ;;
-  *)
-    echo "Usage:"
-    echo "dot command"
-  ;;
-esac
+      "news")
+        nix-shell --run "home-manager news"
+      ;;
+      *)
+        echo "Usage:"
+        echo "dot command"
+      ;;
+    esac
 
-popd > /dev/null
+    popd > /dev/null
   '';
-in {
+in
+{
   home.packages = [
     dotTool
   ];
