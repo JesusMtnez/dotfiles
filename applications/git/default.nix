593251e383fe42d8d2bdeeaeb6600fa944d70c31
JesusMtnez <jesusmartinez93@gmail.com>
2022-04-16 20:41:06 +0200

    migrate gitignore to nix

--- applications/git/default.nix
+++ applications/git/default.nix
@@ -1,12 +1,30 @@
 { pkgs, ... }:
 {
+  programs.git = {
+    enable = true;
+    package = pkgs.gitAndTools.gitFull;
+
+    ignores = [
+      # Mac OSX
+      ".DS_Store"
+
+      # Metals
+      ".metals/"
+      ".bloop/"
+      "project/metals.sbt"
+      ".bsp/"
+
+      # Editors
+      ".idea/"
+      ".vscode/"
+    ];
+  };
+
+  home.file.".gitconfig".source = ./gitconfig;
+
   home.packages = with pkgs; [
-    git
     gitAndTools.diff-so-fancy
     gitAndTools.gh
     gitAndTools.glab
   ];
-
-  home.file.".gitconfig".source = ./gitconfig;
-  home.file.".gitignore".source = ./gitignore;
 }
