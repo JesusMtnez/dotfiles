ecf5cfa22e8fa351b19bbc17156a55006cb88ad3
JesusMtnez <jesusmartinez93@gmail.com>
2022-09-07 05:39:47 +0200

    update flake.lock and nix-darwin homebrew

--- hosts/sirius/homebrew.nix
+++ hosts/sirius/homebrew.nix
@@ -2,59 +2,60 @@
 {
   homebrew = {
     enable = true;
-    autoUpdate = true;
-    cleanup = "zap";
+    onActivation.autoUpdate = true;
+    onActivation.upgrade = true;
+    onActivation.cleanup = "zap";
     global.brewfile = true;
-    global.noLock = true;
+    global.lockfiles = true;
 
     taps = [
-      "homebrew/bundle"
-      "homebrew/cask"
-      "homebrew/cask-drivers"
-      "homebrew/cask-fonts"
-      "homebrew/core"
-      "homebrew/services"
+      { name = "homebrew/bundle"; }
+      { name = "homebrew/cask"; }
+      { name = "homebrew/cask-drivers"; }
+      { name = "homebrew/cask-fonts"; }
+      { name = "homebrew/core"; }
+      { name = "homebrew/services"; }
     ];
 
     brews = [
-      "pinentry"
-      "gnupg"
-      "protobuf"
-      "switchaudio-osx"
-      "watch"
+      { name = "pinentry"; }
+      { name = "gnupg"; }
+      { name = "protobuf"; }
+      { name = "switchaudio-osx"; }
+      { name = "watch"; }
     ];
 
     casks = [
-      "amethyst"
-      "brave-browser"
-      "calibre"
-      "coconutbattery"
-      "dbeaver-community"
-      "discord"
-      "dropbox"
-      "firefox"
-      "font-sf-pro"
-      "google-chrome"
-      "graphql-playground"
-      "hiddenbar"
-      "iterm2"
-      "itsycal"
-      "joplin"
-      "karabiner-elements"
-      "keepassxc"
-      "mongodb-compass"
-      "mounty"
-      "notion"
-      "openvpn-connect"
-      "raycast"
-      "shottr"
-      "slack"
-      "spotify"
-      "syncthing"
-      "synology-drive"
-      "vlc"
-      "whatsapp"
-      "zoom"
+      { name = "amethyst"; }
+      { name = "brave-browser"; }
+      { name = "calibre"; }
+      { name = "coconutbattery"; }
+      { name = "dbeaver-community"; }
+      { name = "discord"; }
+      { name = "dropbox"; }
+      { name = "firefox"; }
+      { name = "font-sf-pro"; }
+      { name = "google-chrome"; }
+      { name = "graphql-playground"; }
+      { name = "hiddenbar"; }
+      { name = "iterm2"; }
+      { name = "itsycal"; }
+      { name = "joplin"; }
+      { name = "karabiner-elements"; }
+      { name = "keepassxc"; }
+      { name = "mongodb-compass"; }
+      { name = "mounty"; }
+      { name = "notion"; }
+      { name = "openvpn-connect"; }
+      { name = "raycast"; }
+      { name = "shottr"; }
+      { name = "slack"; }
+      { name = "spotify"; }
+      { name = "syncthing"; }
+      { name = "synology-drive"; }
+      { name = "vlc"; }
+      { name = "whatsapp"; }
+      { name = "zoom"; }
     ];
   };
 }
