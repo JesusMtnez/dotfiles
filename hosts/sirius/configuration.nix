38bf72227753c5dfba287962cdedc523dbd288d9
JesusMtnez <jesusmartinez93@gmail.com>
2022-10-06 06:29:29 +0200

    sirius/add mac system config

--- hosts/sirius/configuration.nix
+++ hosts/sirius/configuration.nix
@@ -1,5 +1,6 @@
 { config, pkgs, ... }:
 {
+  services.activate-system.enable = true;
   services.nix-daemon.enable = true;
 
   nix.configureBuildUsers = true;
@@ -10,4 +11,25 @@
   };
 
   users.users.jmartinez.home = "/Users/jmartinez";
+
+  system.defaults.dock = {
+    autohide = false;
+    mru-spaces = false;
+    minimize-to-application = true;
+    orientation = "bottom";
+    show-process-indicators = true;
+    show-recents = false;
+    tilesize = 16;
+    static-only = false;
+  };
+
+  system.defaults.trackpad = {
+    Clicking = true;
+    ActuationStrength = 0; # Silent Clicking
+  };
+
+  system.keyboard = {
+    enableKeyMapping = true;
+    remapCapsLockToControl = true;
+  };
 }
