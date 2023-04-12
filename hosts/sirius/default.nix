5112ca8555ef10d4d84b50f9d78d122d0b885734
JesusMtnez <jesusmartinez93@gmail.com>
2023-04-12 09:16:39 +0200

    sirius/remove hm patch for applications in mac

--- hosts/sirius/default.nix
+++ hosts/sirius/default.nix
@@ -34,31 +34,5 @@ in
     awscli2
   ];
 
-  # Home-manager does not link installed applications to the user environment.
-  # https://github.com/nix-community/home-manager/issues/1341
-  home.activation = pkgs.lib.mkIf pkgs.stdenv.isDarwin
-    {
-      addApplications =
-        let
-          apps = pkgs.buildEnv {
-            name = "home-manager-applications";
-            paths = config.home.packages;
-            pathsToLink = "/Applications";
-          };
-        in
-        lib.hm.dag.entryAfter [ "writeBoundary" ] ''
-          baseDir="$HOME/Applications/Home Manager Apps"
-          if [ -d "$baseDir" ]; then
-            rm -rf "$baseDir"
-          fi
-          mkdir -p "$baseDir"
-          for appFile in ${apps}/Applications/*; do
-            target="$baseDir/$(basename "$appFile")"
-            $DRY_RUN_CMD cp ''${VERBOSE_ARG:+-v} -fHRL "$appFile" "$baseDir"
-            $DRY_RUN_CMD chmod ''${VERBOSE_ARG:+-v} -R +w "$target"
-          done
-        '';
-    };
-
   home.stateVersion = "22.11";
 }
