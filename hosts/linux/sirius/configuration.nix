141dca1f2dff0783647fceb37735def87b16e31f
JesusMtnez <jesusmartinez93@gmail.com>
2023-06-01 12:08:33 +0200

    better system configuration

--- hosts/linux/sirius/configuration.nix
+++ hosts/linux/sirius/configuration.nix
@@ -1,31 +1,20 @@
-# Edit this configuration file to define what should be installed on
-# your system.  Help is available in the configuration.nix(5) man page
-# and in the NixOS manual (accessible by running ‘nixos-help’).
-
 { config, pkgs, ... }:
 
 {
-  imports =
-    [ # Include the results of the hardware scan.
-      ./hardware-configuration.nix
-    ];
+  imports = [ ./hardware-configuration.nix ];
 
-  # Bootloader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
   boot.loader.efi.efiSysMountPoint = "/boot/efi";
 
-  networking.hostName = "sirius"; # Define your hostname.
-  networking.wireless.enable = false;  # Enables wireless support via wpa_supplicant.
+  networking.hostName = "sirius";
+  networking.wireless.enable = false;
   networking.firewall.enable = false;
   networking.networkmanager.enable = true;
 
-  # Set your time zone.
   time.timeZone = "Europe/Madrid";
 
-  # Select internationalisation properties.
   i18n.defaultLocale = "en_US.UTF-8";
-
   i18n.extraLocaleSettings = {
     LC_ADDRESS = "es_ES.UTF-8";
     LC_IDENTIFICATION = "es_ES.UTF-8";
@@ -38,40 +27,28 @@
     LC_TIME = "es_ES.UTF-8";
   };
 
-  # Enable the X11 windowing system.
   services.xserver.enable = true;
-
-  # Enable the KDE Plasma Desktop Environment.
   services.xserver.displayManager.sddm.enable = true;
   services.xserver.desktopManager.plasma5.enable = true;
 
-  # Configure keymap in X11
   services.xserver = {
     layout = "us";
     xkbVariant = "mac";
   };
 
-  # Enable CUPS to print documents.
-  services.printing.enable = false;
+  services.xserver.libinput.enable = true;
 
   # Enable sound with pipewire.
-  sound.enable = false;
+  sound.enable = true;
   hardware.pulseaudio.enable = false;
   security.rtkit.enable = true;
   services.pipewire = {
-    enable = false;
-    alsa.enable = false;
+    enable = true;
+    alsa.enable = true;
     alsa.support32Bit = true;
-    pulse.enable = false;
-    #jack.enable = true;
-
-    # use the example session manager (no others are packaged yet so this is enabled by default,
-    # no need to redefine it in your config for now)
-    #media-session.enable = true;
+    pulse.enable = true;
+    jack.enable = true;
   };
-
-  # Enable touchpad support (enabled default in most desktopManager).
-  # services.xserver.libinput.enable = true;
 
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.jesus = {
@@ -85,8 +62,6 @@
   services.xserver.displayManager.autoLogin.enable = true;
   services.xserver.displayManager.autoLogin.user = "jesus";
 
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
   environment.systemPackages = with pkgs; [
     libsForQt5.ark
 
@@ -95,11 +70,16 @@
     neovim
   ];
 
-  # programs.mtr.enable = true;
+  environment.plasma5.excludePackages = with pkgs; [
+    libsForQt5.elisa
+    kate
+    libsForQt5.khelpcenter
+    konsole
+  ];
 
   services.openssh.enable = true;
 
   services.spice-vdagentd.enable = true;
-  
+
   system.stateVersion = "23.05";
 }
