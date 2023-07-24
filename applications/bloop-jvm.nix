1c4c0f9ef2d609b7c3c065a7ed110dc2eb6c045f
JesusMtnez <jesusmartinez93@gmail.com>
2023-07-24 12:24:57 +0200

    feat(app): add bloop installation using jvm version

---
+++ applications/bloop-jvm.nix
@@ -0,0 +1,44 @@
+{ lib, stdenv, jre, coursier, makeWrapper, setJavaClassPath }:
+
+let
+  baseName = "bloop";
+  version = "1.5.9";
+  deps = stdenv.mkDerivation {
+    name = "${baseName}-deps-${version}";
+    buildCommand = ''
+      export COURSIER_CACHE=$(pwd)
+      ${coursier}/bin/cs fetch ch.epfl.scala:bloopgun-core_2.13:${version} > deps
+      mkdir -p $out/share/java
+      cp $(< deps) $out/share/java/
+    '';
+    outputHashMode = "recursive";
+    outputHash = "sha256-umMtQtPi7mT1lOniYBwTAM9KqNGwzGwuCl8zfFX82tE=";
+  };
+in
+stdenv.mkDerivation {
+  pname = baseName;
+  inherit version;
+
+  nativeBuildInputs = [ makeWrapper setJavaClassPath ];
+  buildInputs = [ deps ];
+
+  dontUnpack = true;
+
+  installPhase = ''
+    runHook preInstall
+
+    makeWrapper ${jre}/bin/java $out/bin/${baseName} \
+      --add-flags "-cp $CLASSPATH bloop.bloopgun.Bloopgun"
+
+    runHook postInstall
+  '';
+
+  installCheckPhase = ''
+    $out/bin/${baseName} --version | grep -q "${version}"
+  '';
+
+  meta = with lib; {
+    description = "Opinionated code formatter for Scala";
+    homepage = "http://scalameta.org/bloop";
+  };
+}
