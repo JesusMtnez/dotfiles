ce65a4955f38d8382a8f6f1bbe401fb80b927c35
JesusMtnez <jesusmartinez93@gmail.com>
2025-09-06 07:23:46 +0200

    ci(sleek): bump sleek 2.0.18 -> 2.0.20

--- packages/sleek.nix
+++ packages/sleek.nix
@@ -1,49 +1,37 @@
-{ lib, fetchurl, appimageTools, }:
+{ lib, fetchurl, appimageTools, makeWrapper, }:
 
 let
   pname = "sleek";
-  version = "2.0.18";
+  version = "2.0.20";
 
   src = fetchurl {
     url = "https://github.com/ransome1/sleek/releases/download/v${version}/sleek-${version}-linux-x86_64.AppImage";
-    hash = "sha256-t7efmLCNzg3wYWDKNCASzHXyBPgaMnwPAMXpu0zjKDc=";
+    hash = "sha256-WgElz5irqtbXj/EKVnAv1qSvaUicDG1ifRziJ/Cks5Y=";
   };
 
   appimageContents = appimageTools.extract {
     inherit pname version src;
   };
-in
-appimageTools.wrapType2 {
-  inherit pname version src;
-
-  profile = ''
-    export LC_ALL=C.UTF-8
-  '';
-
-  multiPkgs = null; # no 32bit needed
-
-  extraPkgs = pkgs: (appimageTools.defaultFhsEnvArgs.multiPkgs pkgs)
-    ++ [ pkgs.libappindicator ];
-
-  extraInstallCommands = ''
-    # Strip version from binary name.
-    # mv $out/bin/${pname}-${version} $out/bin/${pname}
-
-    install -m 444 \
-        -D ${appimageContents}/sleek.desktop \
-        $out/share/applications/sleek.desktop
-
-    substituteInPlace \
-        $out/share/applications/sleek.desktop \
-        --replace-fail 'Exec=AppRun' 'Exec=${pname}'
-
-    cp -r ${appimageContents}/usr/share/icons $out/share
-  '';
 
   meta = with lib; {
     description = "todo.txt manager";
-    homepage = "https://github.com/ransome1/sleek/wiki";
+    homepage = "https://github.com/ransome1/sleek";
     license = licenses.mit;
     platforms = [ "x86_64-linux" ];
   };
+in
+appimageTools.wrapType2 {
+  inherit pname version src meta;
+
+  nativeBuildInputs = [ makeWrapper ];
+
+  extraInstallCommands = ''
+    wrapProgram $out/bin/sleek \
+        --add-flags "\''${NIXOS_OZONE_WL:+\''${WAYLAND_DISPLAY:+--ozone-platform-hint=auto --enable-features=WaylandWindowDecorations}}"
+    mkdir -p $out/share/{applications,sleek}
+    cp -a ${appimageContents}/{locales,resources} $out/share/sleek
+    cp -a ${appimageContents}/usr/share/icons $out/share
+    install -Dm 444 ${appimageContents}/sleek.desktop $out/share/applications
+    substituteInPlace $out/share/applications/sleek.desktop --replace-warn 'Exec=AppRun' 'Exec=${pname}'
+  '';
 }
