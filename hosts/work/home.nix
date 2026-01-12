57ac5beeee602ab577591132d85fddc4bebbbf9c
JesusMtnez <jesusmartinez93@gmail.com>
2026-01-12 10:04:42 +0100

    fix(work): use always jdk for java in work host

--- hosts/work/home.nix
+++ hosts/work/home.nix
@@ -6,7 +6,7 @@
   nixpkgs = {
     config.allowUnfree = true;
     config.packageOverrides = {
-      jre = pkgs.jre25_minimal;
+      jre = pkgs.jdk25_headless;
       jdk = pkgs.jdk25_headless;
     };
   };
